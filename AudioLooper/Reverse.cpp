7801f5a94220907354bab434b9931250a6942be9
Andre O <59809568+forced-badname@users.noreply.github.com>
2021-07-30 18:11:13 -0700

    reverse function complete

--- AudioLooper/Reverse.cpp
+++ AudioLooper/Reverse.cpp
@@ -10,6 +10,7 @@ using std::endl;
 string reverse(string path)
 {
 	sf::SoundBuffer buffer;
+
 	buffer.loadFromFile(path);
 
     unsigned int SampleRate = buffer.getSampleRate();
@@ -18,8 +19,8 @@ string reverse(string path)
     std::size_t Count = buffer.getSampleCount();
 
     cout << "SampleRate: " << SampleRate << "\nChannels: " << Channels << "\nDuration: " << Duration.asSeconds() << "\nCount: " << Count << endl;
-    const sf::Int16* Samples = buffer.getSamples();
 
+    const sf::Int16* Samples = buffer.getSamples();
     std::vector<sf::Int16> reversedSamples;
 
     for (int i = Count - 1; i >= 0; i--)
@@ -27,9 +28,13 @@ string reverse(string path)
         reversedSamples.push_back(Samples[i]);
     }
 
-    buffer.loadFromSamples(&reversedSamples[0], reversedSamples.size(), Channels, 44100);
+    buffer.loadFromSamples(&reversedSamples[0], reversedSamples.size(), Channels, SampleRate);
+
+    path.insert((path.length() - 4), " rvrsd");
 
     cout << "path: " << path << endl;
+
+    buffer.saveToFile(path);
 
     return path;
 
